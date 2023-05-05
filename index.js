5ca9ddaa6e5ca14ab2f86e718911f2fa896f2a57
unchaptered <workstation19961002@gmail.com>
2023-05-05 16:40:15 +0900

    Update(Electron) : new ipc codes in application

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const electron = require('electron');
+const ffmpeg = require('fluent-ffmpeg');
 
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow, ipcMain } = electron;
 
 app.on('ready', () => {
 
     const mainWindow = new BrowserWindow({});
     mainWindow.loadURL(`file://${__dirname}/index.html`);
 
+});
+
+
+ipcMain.on('video:submit', (event, path) => {
+   
+    ffmpeg.ffprobe(path, (err, metadata) => {
+        console.log('Video Duration is : ', metadata.format.duration);
+    });
+    console.log('hello, world');
+    
 });
