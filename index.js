f8f6cc294595ec4c95889ce26769c0a0e3313876
unchaptered <workstation19961002@gmail.com>
2023-05-05 16:48:58 +0900

    Update(Electron) : fin of IPC

--- index.js
+++ index.js
@@ -3,19 +3,27 @@ const ffmpeg = require('fluent-ffmpeg');
 
 const { app, BrowserWindow, ipcMain } = electron;
 
+let mainWindow;
+
 app.on('ready', () => {
 
-    const mainWindow = new BrowserWindow({});
+    mainWindow = new BrowserWindow({});
     mainWindow.loadURL(`file://${__dirname}/index.html`);
 
 });
 
 
 ipcMain.on('video:submit', (event, path) => {
+
+    console.log('video:submit');
    
     ffmpeg.ffprobe(path, (err, metadata) => {
-        console.log('Video Duration is : ', metadata.format.duration);
+         console.log('Video Duration is : ', metadata.format.duration);
+
+         mainWindow.webContents.send(
+            'video:metadata',
+            metadata.format.duration
+         );
     });
-    console.log('hello, world');
     
 });
