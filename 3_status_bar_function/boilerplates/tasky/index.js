6a9336c55f789f701b2dd1111b456cab65ab876a
unchaptered <workstation19961002@gmail.com>
2023-05-05 21:59:59 +0900

    Create(Electron) : preview interface for window

--- 3_status_bar_function/boilerplates/tasky/index.js
+++ 3_status_bar_function/boilerplates/tasky/index.js
@@ -1,26 +1,58 @@
 const path = require('path');
 const electron = require('electron');
-const { app } = electron;
+
+const { app, ipcMain, BrowserWindow } = electron;
 
 const { TimerTray } = require('./timerTray');
 const { MainWindow } = require('./mainWindow');
+const { SubWindow } = require('./subWindow');
 
 const platform = process.platform;
 if (platform === ('darwin' || 'win32')) return app.quit();
 
-let mainWindow;
-let timerTray;
+/** @type { MainWindow } */ let mainWindow;
+/** @type { SubWindow } */ let subWindow;
+/** @type { TimerTray } */ let timerTray;
 
 app.on('ready', () => {
 
     if (platform === 'darwin') app.dock.hide();
 
     const traySize = { width: 300, height: 600 };
-    mainWindow = new MainWindow(traySize)
-    mainWindow.loadURL(`file://${__dirname}/src/index.html`)
+    mainWindow = new MainWindow(traySize);
+    mainWindow.loadURL(`file://${__dirname}/src/index.html`);
+
+    // const subWindow = new BrowserWindow({
+    //     width: traySize.width,
+    //     height: traySize.height,
+    //     show: true,
+    //     frame: false,
+    //     resizable: false,
+    // });
+    const { screen } = require('electron');
+
+    const displays = screen.getAllDisplays();
+    const display = displays[0];
+    const { workArea } = display;
+    const { width: workAreaWidth, height: workAreaHeight } = workArea;
+
+    const subTraySize = { width: 300, height: 50};
+
+    console.log(workAreaWidth - subTraySize.width, workAreaHeight - subTraySize.height - 5);
     
+    subWindow = new SubWindow(subTraySize);
+    subWindow.loadURL(`file://${__dirname}/src/sub_index.html`);
+    subWindow.setPosition(workAreaWidth - subTraySize.width, workAreaHeight - subTraySize.height - 5);
+    subWindow.show();
+
     const iconName = platform === 'win32' ? 'windows-icon.png' : 'iconTemplate.png';
     const iconPath = path.join(__dirname, `./src/assets/${iconName}`); 
     timerTray = new TimerTray(iconPath, traySize, mainWindow);
 
+});
+
+
+ipcMain.on('update-timer', (event, timerLeftTime) => {
+    console.log('야옹', timerLeftTime);
+    subWindow.webContents.send('update-timer:preview', timerLeftTime);
 });
---
+++ 3_status_bar_function/boilerplates/tasky/subWindow.js
@@ -0,0 +1,31 @@
+const { Menu, BrowserWindow } = require("electron");
+
+class SubWindow extends BrowserWindow {
+
+    /** @param {{ width: number, height: number }} options  */
+    constructor(subTraySize) {
+        super({
+            title:'title',
+            width: subTraySize.width,
+            height: subTraySize.height,
+            show: false,
+            frame: false,
+            transparent: true,
+            hasShadow: false,
+            resizable: false,
+            webPreferences: {
+                nodeIntegration: true,
+                contextIsolation:false
+            }
+        });
+
+        this.setMenu(Menu.buildFromTemplate([]))
+
+    }
+
+}
+
+
+module.exports = {
+    SubWindow
+}
