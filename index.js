89ab534807cc3b2d4f22bacc9b03d07e6d0f6079
unchaptered <workstation19961002@gmail.com>
2023-05-05 17:25:15 +0900

    Update(Electron) : function to open new window in same application

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const electron = require('electron');
 const { app, BrowserWindow, Menu } = electron;
 
 let mainWindow;
+let addWindow;
 
 app.on('ready', () => {
     mainWindow = new BrowserWindow({});
@@ -11,6 +12,15 @@ app.on('ready', () => {
     const mainMenu = Menu.buildFromTemplate(menuTemplate);
     Menu.setApplicationMenu(mainMenu);
 });
+
+function createAddWindow() {
+    addWindow = new BrowserWindow({
+        width: 300,
+        height: 200,
+        title: 'Add New Todo'
+    });
+    addWindow.loadURL(`file://${__dirname}/add.html`);
+}
 
 // Hoisting
 const platform = process.platform;
@@ -21,17 +31,14 @@ const menuTemplate = [
         label: 'File',
         submenu: [
             {
-                label: 'New Todo'
+                label: 'New Todo',
+                click() {
+                    createAddWindow();
+                }
             },
             {
                 label: 'quit',
-                accelerator: (() => {
-                    if (process.platform === 'darwin') {
-                        return 'Command+Q';
-                    } else {
-                        return 'Ctrl+Q';
-                    }
-                })(),
+                accelerator: platform === 'darwin' ? 'Command+Q' : 'Ctrl+Q',
                 click() {
                     app.quit();
                 }
@@ -39,5 +46,3 @@ const menuTemplate = [
         ]
     }
 ];
-
-// if (process.platform !== 'darwin') menuTemplate.shift();
