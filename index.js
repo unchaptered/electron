7f03472067bc97c2cd80a67087d5816e1df68424
unchaptered <workstation19961002@gmail.com>
2023-05-05 17:06:29 +0900

    Update(Electron) : replace base menu to custom menu

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const electron = require('electron');
 
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow, Menu } = electron;
 
 let mainWindow;
 
 app.on('ready', () => {
     mainWindow = new BrowserWindow({});
     mainWindow.loadURL(`file://${__dirname}/index.html`);
+
+    const mainMenu = Menu.buildFromTemplate(menuTemplate);
+    Menu.setApplicationMenu(mainMenu);
 });
+
+// Hoisting
+const menuTemplate = [
+    {
+        label: 'File',
+        submenu: [
+            {
+                label: 'New Todo'
+            }
+        ]
+    }
+];
