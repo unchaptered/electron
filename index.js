fd3c3c80bf54acfad777ae7d7f34c3f4eb943693
unchaptered <workstation19961002@gmail.com>
2023-05-05 16:12:36 +0900

    Update(Electron) : load index.html

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ const { app, BrowserWindow } = electron;
 app.on('ready', () => {
 
     const mainWindow = new BrowserWindow({});
-    mainWindow.loadURL('http://google.com');
+    mainWindow.loadURL(`file://${__dirname}/index.html`);
 
 });
