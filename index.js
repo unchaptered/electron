888176ce40499bea3414e06236fc586a4ca84ca9
unchaptered <workstation19961002@gmail.com>
2023-05-05 16:54:18 +0900

    Simplify(Electron) : unnecessary codes

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const { app, BrowserWindow, ipcMain } = electron;
 let mainWindow;
 
 app.on('ready', () => {
-
     mainWindow = new BrowserWindow({
         webPreferences: {
             nodeIntegration: true,
@@ -14,21 +13,12 @@ app.on('ready', () => {
         }
     });
     mainWindow.loadURL(`file://${__dirname}/index.html`);
-
 });
 
 
 ipcMain.on('video:submit', (event, path) => {
-
-    console.log('video:submit');
-   
     ffmpeg.ffprobe(path, (err, metadata) => {
-         console.log('Video Duration is : ', metadata.format.duration);
-
-         mainWindow.webContents.send(
-            'video:metadata',
-            metadata.format.duration
-         );
+        const duration = metadata.format.duration;
+        mainWindow.webContents.send('video:metadata', duration);
     });
-    
 });
